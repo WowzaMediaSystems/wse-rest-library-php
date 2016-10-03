556b1904841d82b8b451f844a43f70a13dcf8bf8
Matt Young <myoung@wowza.com>
2016-10-03 10:07:14 -0400

    Fixed query params issue.

--- lib/class.dvrclipextraction.php
+++ lib/class.dvrclipextraction.php
@@ -39,17 +39,19 @@ class DvrClipExtraction extends Wowza{
 	 */
 	public function convert($name, $startTime=null, $endTime=null, $outputFileName = null){
 		$this->setNoParams();
-		$query = "?";
+		$query = "";
 		if(!is_null($startTime)){
 			$query.="dvrConverterStartTime=".$startTime;
 		}
 		if(!is_null($endTime)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterEndTime=".$endTime;
 		}
 		if(!is_null($outputFileName)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterOutputFilename=".$outputFileName;
 		}
-		$query = (strlen($query)==1)?"":$query;
+		$query = (strlen($query)==0)?"":"?".$query;
 	
 		$this->restURI = $this->restURI."/{$name}/actions/convert{$query}";
 		return $this->sendRequest($this->preparePropertiesForRequest(),array(), self::VERB_PUT);
@@ -70,17 +72,19 @@ class DvrClipExtraction extends Wowza{
 	 */
 	public function convertByDurationWithStartTime($name, $startTime,$duration, $outputFileName = null){
 		$this->setNoParams();
-		$query = "?";
+		$query = "";
 		if(!is_null($startTime)){
 			$query.="dvrConverterStartTime=".$startTime;
 		}
 		if(!is_null($duration)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterDuration=".$duration;
 		}
 		if(!is_null($outputFileName)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterOutputFilename=".$outputFileName;
 		}
-		$query = (strlen($query)==1)?"":$query;
+		$query = (strlen($query)==1)?"":"?".$query;
 		$this->restURI = $this->restURI."/{$name}/actions/convert{$query}";
 	
 		return $this->sendRequest($this->preparePropertiesForRequest(),array(), self::VERB_PUT);
@@ -88,17 +92,19 @@ class DvrClipExtraction extends Wowza{
 
 	public function convertByDurationWithEndTimeTime($name, $endTime, $duration, $outputFileName = null){
 		$this->setNoParams();
-		$query = "?";
+		$query = "";
 		if(!is_null($endTime)){
 			$query.="dvrConverterEndTime=".$endTime;
 		}
 		if(!is_null($duration)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterDuration=".$duration;
 		}
 		if(!is_null($outputFileName)){
+			if(!empty($query)){ $query.= "&"; }
 			$query.="dvrConverterOutputFilename=".$outputFileName;
 		}
-		$query = (strlen($query)==1)?"":$query;
+		$query = (strlen($query)==1)?"":"?".$query;
 		$this->restURI = $this->restURI."/{$name}/actions/convert{$query}";
 	
 		return $this->sendRequest($this->preparePropertiesForRequest(),array(), self::VERB_PUT);
