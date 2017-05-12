708dfa731b9bcf172c8c891c8ea13353e2608175
Matt Young <myoung@wowza.com>
2017-05-12 13:42:40 -0400

    Updated transcoder config entity to be php7 compatible and updated some of the class variables to ensure compatibility with the latest version of the REST API.

--- lib/entities/application/class.transcoderconfig.php
+++ lib/entities/application/class.transcoderconfig.php
@@ -5,16 +5,14 @@
 //
 namespace com\wowza\entities\application;
 class TranscoderConfig extends \com\wowza\entities\Entity{
-	public $available = array();
-	public $licensed = array();
-	public $licenses = array();
-	public $licensesInUse = array();
-	public $templates = array();
-	public $templatesInUse = "\$\{SourceStreamName\}\.xml,transrate\.xml";
-	public $profileDir = "\$\{com\.wowza\.wms\.context\.VHostConfigHome\}/transcoder/profiles";
-	public $templateDir = "\$\{com\.wowza\.wms\.context\.VHostConfigHome\}/transcoder/templates";
-	public $createTemplateDir = array();
-	public function setURI(){
-		$this->restURI = $baseURI."/transcoder";
-	}
+    public $available = true;
+    public $liveStreamTranscoder = "transcoder";
+    public $Templates = "\$\{SourceStreamName\}\.xml,transrate\.xml";
+    public $profileDir = "\$\{com\.wowza\.wms\.context\.VHostConfigHome\}/transcoder/profiles";
+    public $templateDir = "\$\{com\.wowza\.wms\.context\.VHostConfigHome\}/transcoder/templates";
+    public $createTemplateDir = true;
+    public function setURI($baseURI){
+        $this->restURI = $baseURI."/transcoder";
+    }
 }
+
