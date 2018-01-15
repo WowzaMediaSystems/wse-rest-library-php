0b35f212b08aaf36e448d2795c8911c582303d5d
Roger Littin <roger@wowza.com>
2018-01-15 13:42:26 +1300

    Updated copyright comments

--- src/Entities/Application/StreamConfig.php
+++ src/Entities/Application/StreamConfig.php
@@ -1,6 +1,6 @@
 <?php
 //
-// This code and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC. All rights reserved.
+// This code and all components (c) Copyright 2006 - 2018, Wowza Media Systems, LLC. All rights reserved.
 // This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
 //
 namespace Com\Wowza\Entities\Application;
